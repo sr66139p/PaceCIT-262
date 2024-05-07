c21f5e1b60853ba18a8154ce05af4673877b17fb
Sebastian <sr66139p@pace.edu>
2024-05-06 23:08:29 -0400

    added comments

--- final/wackywords.py
+++ final/wackywords.py
@@ -1,19 +1,22 @@
+#imports QT6 and necessary modules
 from PyQt6 import QtCore, QtGui, QtWidgets
 import random
 import re
 import sys
 
 def read_rand_prompt():
-    # read in a random prompt
+    #read in a random prompt from the text file
     with open("prompts.txt", "r") as prompts_file:
         prompts = prompts_file.readlines()
         rand_i = random.randint(0, len(prompts)-1)
         return prompts[rand_i]
 
+#takes the placeholders out of the prompts and returns them
 def deconstruct_prompt(prompt):
     placeholders = re.findall('{(.*?)}', prompt)
     return placeholders  
 
+#switches between the states of this finite state program
 def b_clicked():
     global state
     # handler for button
@@ -27,6 +30,7 @@ def b_clicked():
         # change state 
         state = 0
     
+#switches to state 0
 def state_0():
     global prompt, ph
     clear_inputs()
@@ -40,6 +44,7 @@ def state_0():
     input2.setPlaceholderText(ph[1])
     input3.setPlaceholderText(ph[2])
     
+#clears the text boxes
 def clear_inputs():
     input1.setPlaceholderText("")
     input2.setPlaceholderText("")
@@ -48,6 +53,7 @@ def clear_inputs():
     input2.setText("")
     input3.setText("")
 
+#switches to state 1
 def state_1():
     global prompt
     # get text inputs
@@ -64,10 +70,12 @@ def state_1():
 
     clear_inputs()
 
+#starts on state 0
 state = 0
 prompt = ''
 ph = []
 
+#adds window properties
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 MainWindow.show()
@@ -78,6 +86,7 @@ MainWindow.setTabShape(QtWidgets.QTabWidget.TabShape.Rounded)
 centralwidget = QtWidgets.QWidget(parent=MainWindow)
 centralwidget.setObjectName("centralwidget")
 
+#declares the next button
 nextbutton = QtWidgets.QPushButton(parent=centralwidget)
 nextbutton.setGeometry(QtCore.QRect(220, 390, 201, 51))
 font = QtGui.QFont()
@@ -89,6 +98,7 @@ nextbutton.setFlat(False)
 nextbutton.setObjectName("nextbutton")
 nextbutton.clicked.connect(b_clicked)
 
+#Declares the 3 input boxes
 input1 = QtWidgets.QTextEdit(parent=centralwidget)
 input1.setGeometry(QtCore.QRect(9, 310, 203, 61))
 input1.setObjectName("input1")
@@ -101,11 +111,13 @@ input3 = QtWidgets.QTextEdit(parent=centralwidget)
 input3.setGeometry(QtCore.QRect(430, 310, 203, 61))
 input3.setObjectName("input3")
 
+#Adds a border for the sake of visibility
 border = "QWidget {border: 1px solid white; }"
 input1.setStyleSheet(border)
 input2.setStyleSheet(border)
 input3.setStyleSheet(border)
 
+#Declares output label
 outputlabel = QtWidgets.QLabel(parent=centralwidget)
 outputlabel.setGeometry(QtCore.QRect(9, 30, 622, 241))
 font = QtGui.QFont()
